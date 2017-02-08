f69a4a75e181b90501b7c6c6abc9d408d0e3d6d5
Hua Hou <hhou2011@gmail.com>
2017-02-08 11:48:39 -0800

    Fix the bug on restoreLastSession.

--- src/main/java/org/dsa/iot/knx/KnxLink.java
+++ src/main/java/org/dsa/iot/knx/KnxLink.java
@@ -136,9 +136,9 @@ public class KnxLink {
 			Value interval = child.getAttribute(ATTR_POLLING_INTERVAL);
 			Value timeout = child.getAttribute(ATTR_POLLING_TIMEOUT);
 			KnxIPConnection ipConnection;
-			if (transType != null && groupLevel != null && null != deviceAddress && interval != null
-					&& timeout != null) {
-				if (localHost != null && remoteHost != null && port != null && useNat != null) {
+			if (transType != null && groupLevel != null && interval != null && timeout != null) {
+				if (localHost != null && remoteHost != null && port != null && useNat != null
+						&& null != deviceAddress) {
 					ipConnection = new KnxIPTunnelingConnection(getLink(), child);
 				} else {
 					ipConnection = new KnxIPRoutingConnection(getLink(), child);
